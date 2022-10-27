6faa5b128e0094401b133ca620414c074375ffc1
milan-infostride <milansinghdav@gmail.com>
2022-10-27 15:05:11 +0530

    saleController modified

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -7,12 +7,21 @@ const salesController = {
 
    async AddSale(req,res,next){
         try{
+            let adder = await Agent.findById(req.user_id);
             let reqAgent = await Agent.findOne({email: req.body.Agent_Email})
+            if(adder.role!='admin'){
+                if(adder._id.toString()!=reqAgent._id.toString()){
+                    let newError = new Error();
+                    newError.message = errors.authenticationError
+                    throw newError;
+                }
+            }
             if(!reqAgent){
                 let error = new Error();
                 error.message = errors.AgentNotFound
                 throw error;
             }
+            
             let newSales = new Sales({
                 ...req.body,
                 Agent_id: reqAgent._id
